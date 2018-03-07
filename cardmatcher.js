aa2c5e9fb4a0ba1b577908c5d592d78e98057fe8
John Kosmicke <kozjob@yahoo.com>
2018-03-07 14:12:10 -0800

    update

--- cardmatcher.js
+++ cardmatcher.js
@@ -3,6 +3,7 @@
 $(document).ready( function(){
 	$("#grid-size").focus();
 	$("#grid-size-submit").click( function(e){
+		console.log("here");
 		e.preventDefault();
 		var size = $("#grid-size").val();
 		matcherController.init(size);
